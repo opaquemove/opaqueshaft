05bcd4e1f28a08ddeeb7bf4f9989e42b21fb900d
Masato.Nakanishi <opaquemove@gmail.com>
2020-06-27 15:03:46 +0900

    OpaqueShaft 27.jun2020-1

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+//'use strict'
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
 
+var ipaddr = "0.0.0.0";
+var port   = process.env.PORT || 8080;
+
+app.use('/', express.static( __dirname + '/public' ));
+
+server.on( 'listening', () => {
+  console.log('listening on ' + port );
+  console.log('__dirname:' + __dirname );
+  console.log('server:' + ipaddr + ':' + port );
+});
+
+server.listen( port, ipaddr );
+
+/*
 app.get('/', (req, res) => res.send('OpaqueShaft.'));
 app.listen( port, () => console.log(`OpaqueShaft listening on port ${port}.`));
+*/
 
