fcb2cf1ddad3c7ff77ef6556c79ccd778891009a
Masato.Nakanishi <opaquemove@gmail.com>
2020-06-28 22:33:28 +0900

    OpaqueShaft 28.jun.2020-12

--- app.js
+++ app.js
@@ -2,12 +2,23 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io     = require('socket.io')(server);
 
 var ipaddr = "0.0.0.0";
 var port   = process.env.PORT || 8080;
 
 app.use('/', express.static( __dirname + '/public' ));
 
+io.on( 'connection', ( socket ) => {
+  socket.on('cmd', ( msg ) => {
+    switch( msg ) {
+      default:
+        socket.broadcast.emit('cmd', msg );
+        socket.emit( 'cmd', msg );
+        break;
+    }
+  })
+})
 server.on( 'listening', () => {
   console.log('listening on ' + port );
   console.log('__dirname:' + __dirname );
