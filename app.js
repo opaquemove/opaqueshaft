618aedc6f9bd41c430da2af1c06674ad6f77fa7e
Masato.Nakanishi <opaquemove@gmail.com>
2020-06-30 00:36:02 +0900

    OpaqueShaft 29.jun.2020-17

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ var cookieParser = require('cookie-parser');
 var app = express();
 var server = require('http').Server(app);
 var io     = require('socket.io').listen(server);
-io.configure( function() {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10 );
-});
+//io.configure( function() {
+//  io.set("transports", ["xhr-polling"]);
+//  io.set("polling duration", 10 );
+//});
 var ipaddr = "0.0.0.0";
 var port   = process.env.PORT || 8080;
 
