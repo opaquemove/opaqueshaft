91a703930e23ba941a3392a04d6099cd64799625
Masato.Nakanishi <opaquemove@gmail.com>
2020-08-22 17:52:37 +0900

    OpaqueShaft 22.aug.2020-4

--- public/report_dlg.js
+++ public/report_dlg.js
@@ -5,12 +5,15 @@ function ctlReportDlg(){
 
 
 function report_dlg(){
-    this.frame  = null;
-    this.tid    = null;
-    this.top    = 84;
+    this.frame      = null;
+    this.tid        = null;
+    this.top        = 84;
+    this.height     = null;
+    this.counter    = -1;
 
     this.frame      = document.getElementById('REPORT_FRAME');
-    this.close();
+    this.height     = this.frame.offsetHeight;
+    this.hidden();
 
 	var w = document.body.clientWidth;
 	var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
@@ -23,11 +26,10 @@ report_dlg.prototype = {
         var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
         this.frame.style.visibility     = 'visible';
         this.frame.style.top = h + 'px';
-
+        console.log( 'open...');
         this.tid = setInterval( 
             ( function(){
                 var t = parseInt( this.frame.style.top );
-                // console.log( 'opening:' + t );
                 var delta = Math.floor( (t - this.top ) / 3 );
                 this.frame.style.top = ( t - delta ) + 'px';
                 if ( delta <= 4 ){
@@ -41,13 +43,27 @@ report_dlg.prototype = {
         // console.log('stop');
         clearTimeout( this.tid );
     },
+    hidden : function(){
+        this.frame.style.visibility     = 'hidden';
+        console.log('hidden');
+    },
     close : function(){
         var w = document.body.clientWidth;
-        var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
+        this.counter = 1;
+        this.tid = setInterval( 
+            ( function(){
+                var t = parseInt( this.frame.style.top );
+                this.frame.style.top = ( t + this.counter ) + 'px';
+                if ( parseInt( this.frame.style.height ) < ( t + this.counter + 84 ) ){
+                    this.stop();
+                    var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
+                    this.frame.style.top = h + 'px';
+                    this.frame.style.visibility     = 'hidden';
+                    console.log('close');
+                }
+                this.counter = this.counter * 1.2;
+            }).bind( this ) , 30 );
         
-        
-        this.frame.style.visibility     = 'hidden';
-        console.log('close');
     },
     opened : function(){
         return ( this.frame.style.visibility != 'hidden' );
