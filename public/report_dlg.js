b307dfddd5f87c6179c111c88b075fe17741312b
Masato.Nakanishi <opaquemove@gmail.com>
2020-08-22 17:08:48 +0900

    OpaqueShaft 22.aug.2020-3

--- public/report_dlg.js
+++ public/report_dlg.js
@@ -6,6 +6,8 @@ function ctlReportDlg(){
 
 function report_dlg(){
     this.frame  = null;
+    this.tid    = null;
+    this.top    = 84;
 
     this.frame      = document.getElementById('REPORT_FRAME');
     this.close();
@@ -17,11 +19,33 @@ function report_dlg(){
 
 report_dlg.prototype = {
     open : function( ){
+        var w = document.body.clientWidth;
+        var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
         this.frame.style.visibility     = 'visible';
-        this.frame.style.top = '84px';
-        console.log('open');
+        this.frame.style.top = h + 'px';
+
+        this.tid = setInterval( 
+            ( function(){
+                var t = parseInt( this.frame.style.top );
+                // console.log( 'opening:' + t );
+                var delta = Math.floor( (t - this.top ) / 3 );
+                this.frame.style.top = ( t - delta ) + 'px';
+                if ( delta <= 4 ){
+                    this.stop();
+                    this.frame.style.top = this.top + 'px';
+                }
+            }).bind( this ) , 30 );
+
+    },
+    stop : function(){
+        // console.log('stop');
+        clearTimeout( this.tid );
     },
     close : function(){
+        var w = document.body.clientWidth;
+        var h = ( document.body.clientHeight > window.innerHeight )?window.innerHeight : document.body.clientHeight;
+        
+        
         this.frame.style.visibility     = 'hidden';
         console.log('close');
     },
