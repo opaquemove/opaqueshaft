356eae4825a70115e6b0bde94b4c57799879c870
Masato.Nakanishi <opaquemove@gmail.com>
2020-09-04 22:50:27 +0900

    OpaqueShaft 4.sep.2020-10

--- public/index.js
+++ public/index.js
@@ -699,19 +699,19 @@ function locateTimelinebar( e ){
 						// bf.style.border 		= '';
 						// console.log('0');
 					} else {
-						wbf.style.perspective	= '470px';
-						// wb.style.transformStyle	= 'preserve-3d';
-						// wb.style.transform 		= 'translate3d( 0px, 0px, -300px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
-						// wb.style.border 		= '1px solid white';
+						// wbf.style.perspective	= '470px';
+						wb.style.transformStyle	= 'preserve-3d';
+						wb.style.transform 		= 'perspective(470px) translate3d( 0px, 0px, -700px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
+						wb.style.border 		= '1px solid white';
 						wbt.style.transformStyle	= 'preserve-3d';
-						wbt.style.transform 	= 'perspective(470px) translate3d( 0px, 0px, -300px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
+						wbt.style.transform 	= 'perspective(470px) translate3d( 0px, 0px, -700px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
 						wbt.style.border 		= '1px solid white';
 						// wba.style.transformStyle	= 'preserve-3d';
 						// wba.style.transform 	= 'translate3d( 0px, 0px, -350px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
 						// wba.style.border 		= '1px solid red';
-						bo.style.perspective 	= '470px';
+						// bo.style.perspective 	= '470px';
 						bo.style.transformStyle	= 'preserve-3d';
-						bf.style.transform 		= 'translate3d( 0px, 0px, -400px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
+						bf.style.transform 		= 'perspective(470px) translate3d( 0px, 0px, -710px) rotateY(' + ( new_left - tlbOffsetLeft ) + 'deg)';
 						// bf.style.border 		= '1px solid white';
 						// console.log('42');
 					}
