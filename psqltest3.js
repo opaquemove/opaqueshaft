f8b4d80690507f961766c8246cfad4283919b70b
Masato.Nakanishi <opaquemove@gmail.com>
2021-04-29 13:55:25 +0900

    opaqueshaft 2021042904

--- psqltest3.js
+++ psqltest3.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router  = express.Router();
 var pgp     = require('pg-promise')();
-var db      = pgp( process.env.DATABASE_URL );
+var conf = {
+  connectionString : process.env.DATABASE_URL,
+  max : 30,
+  ssl : ssl
+ };
+
+var db      = pgp( conf );
+//var db      = pgp( process.env.DATABASE_URL );
 //
 //  prepare 
 //  $export DATABASE_URL=postgres://[id]:[password]@localhost:5432/opaqueshaft
